435892cb06de7e1951dff7ea592d9ef332955fa8
Azhar <azharbhatti2016@gmail.com>
2020-06-17 13:48:31 +0500

    Java 11 and cleaning of dependencies

---
+++ src/main/java/com/geeks/product/ProductApplication.java
@@ -0,0 +1,35 @@
+package com.geeks.product;
+
+import com.geeks.product.beans.UserBean;
+import com.geeks.product.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class ProductApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		UserBean user = new UserBean();
+		user.setId(1);
+		user.setEmail("azhar@gmail.com");
+		user.setPassword(passwordEncoder.encode("1234"));
+		userRepository.save(user);
+	}
+}
+
+
